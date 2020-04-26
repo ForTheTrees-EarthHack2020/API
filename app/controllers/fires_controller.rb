88000060a7bc7ab1998f605dfb18e8c277624e28
Chew <chew@chew.pw>
2020-04-26 18:28:03 -0500

    no mor eimage code

--- app/controllers/fires_controller.rb
+++ app/controllers/fires_controller.rb
@@ -16,40 +16,40 @@ class FiresController < ApplicationController
       return
     end
 
-    file1 = Tempfile.new(['image', '.png'])
-    File.open(file1.path.to_s, 'wb') do |f|
-      f.write(Base64.decode64(params['picture']))
-    end
+    #file1 = Tempfile.new(['image', '.png'])
+    #File.open(file1.path.to_s, 'wb') do |f|
+    #  f.write(Base64.decode64(params['picture']))
+    #end
 
     # Send code off to the Fire Analyzer
-    response = `python3 predict_fire.py --file=#{file1.path}`
+    #response = `python3 predict_fire.py --file=#{file1.path}`
 
-    if true #response.include? "yes"
+    #if true #response.include? "yes"
       otc = rand(100_000..999_999)
 
-      Aws.config.update(
-        region: 'us-east-2',
-        credentials: Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key))
-      )
+      #Aws.config.update(
+      #  region: 'us-east-2',
+      #  credentials: Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), #Rails.application.credentials.dig(:aws, :secret_access_key))
+      #)
 
-      s3 = Aws::S3::Client.new
-      extension = '.png'
-      o = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
-      string = (0...10).map { o[rand(o.length)] }.join
-      name = "#{string}.png"
-      obj = s3.put_object(
-        bucket: 'for-the-trees',
-        body: file1,
-        acl: 'public-read',
-        key: name
-      )
-      url = "https://for-the-trees.s3.us-east-2.amazonaws.com/#{name}"
+      #s3 = Aws::S3::Client.new
+      #extension = '.png'
+      #o = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
+      #string = (0...10).map { o[rand(o.length)] }.join
+      #name = "#{string}.png"
+      #obj = s3.put_object(
+      #  bucket: 'for-the-trees',
+      #  body: file1,
+      #  acl: 'public-read',
+      #  key: name
+      #)
+      #url = "https://for-the-trees.s3.us-east-2.amazonaws.com/#{name}"
 
-      Fire.create(x: x, y: y, firecode: otc, image: url, userid: user.id, reporter: reporter, phone: phone)
+      Fire.create(x: x, y: y, firecode: otc, image: "", userid: user.id, reporter: reporter, phone: phone)
       json_response({ "status": 'success', 'code': otc }, 201)
-    else
-      json_response({ "status": 'error', 'reason': 'no fire found' }, 400)
-    end
+    #else
+    #  json_response({ "status": 'error', 'reason': 'no fire found' }, 400)
+    #end
   end
 
   # Local Fire Handling
