7e525dfe8f7a01ac6b8997ec0c841d47af292b22
Chew <chew@chew.pw>
2020-04-25 20:47:58 -0500

    add get routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'login', to: 'authorization#login'
   post 'login', to: 'authorization#login'
+  get 'register', to: 'authorization#register'
   post 'register', to: 'authorization#register'
 end
