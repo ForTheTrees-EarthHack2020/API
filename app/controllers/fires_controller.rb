8aa6605d5182d3e5a848f718d30b7ddbd01336e8
Chew <chew@chew.pw>
2020-04-26 13:39:47 -0500

    Start working on new fire method

--- app/controllers/fires_controller.rb
+++ app/controllers/fires_controller.rb
@@ -1,2 +1,19 @@
 class FiresController < ApplicationController
+  # New Fire Handling
+  def new_fire
+    location = params['location']
+    photo = params['photo']
+    phone = params['phone']
+    reporter = params['reporter']
+
+    key = request.headers['Authorization']
+
+    user = User.find_by(api: key)
+    if user.nil?
+      json_response({ "error": 'Auth not valid' }, 401)
+      return
+    end
+
+    # Send code off to the Fire Analyzer
+  end
 end
